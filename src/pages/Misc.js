fa823f6fc75f63ddd12cc8c567a41bd2d41f29d8
Aman Palod <palod.aman@gmail.com>
2022-11-13 20:15:44 -0800

    some typographical changes

--- src/pages/Misc.js
+++ src/pages/Misc.js
@@ -7,16 +7,21 @@ export default function Misc(){
     <div style={{padding:'40px 70px ', textAlign:'center', }}>
         <p>
         I try to keep myself off the radar of social media to be able to tend to my more fulfilling proclivities, like reading. Reading, I believe, is a free source of entertainment that edifies your mind. I mostly read while commuting or before going to bed.
-          </p>
+          I got myself an e-reader recently and it has been a game changer for me. Reading has become more convenient and accessible. </p>
         <p>Following is a list of books, in no particular order, that I have read and/or re-read in 2022, and they have been amazing reads.</p>
     </div >
     <ol style={{padding:"0 50px", marginLeft:"60px"}}>
       <li> Dune, by Frank Herbert</li>
       <li> Tuesdays with Morrie, by Mitch Albom </li>
       <li> Stories of Your Life and Others, by Ed Chiang</li>
+      <li>
+        The Palace of Illusions, by Chitra Banerjee
+      </li>
       <li> To kill a Mockingbird, by Harper Lee </li>
       <li> Dracula, by Bram Stoker</li>
-      <li> Frankenstein, by Mary Shelby</li>
+      <li> The Fifth Season, by NK Jemisin</li>
+      
+     
       <li> Fahrenheit 451, by Ray Bradbury</li>
       <li> Never Let Me Go, by Kazuo Ishiguro</li>
       
@@ -25,14 +30,11 @@ export default function Misc(){
       <li> 1984, by George Orwell</li>
       <li> The Hunger Games trilogy, by Suzzane Collins</li>
       <li> The Bluest Eye, by Toni Morrison</li>
-      <li> The Fifth Season, by NK Jemisin</li>
-      
-
+  
+      <li> Frankenstein, by Mary Shelby</li>
       <li> Don't Be Evil, by Rana Foroohar</li>
       <li> Sapiens, by Yuval Noah Harari</li>
-      <li>
-        The Palace of Illusions, by Chitra Banerjee
-      </li>
+    
     </ol>
     </div>
     </>
